0639fa3baadde7c369a072e3d2915939e6197673
saloum45 <saloumfall45@gmail.com>
2025-06-28 16:48:59 +0000

    in progress

--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Produit
 
 # Create your views here.
+
+# Afficher la liste des produits
+def liste_produits(request):
+    produits = Produit.objects.all()
+    return render(request, 'stock/liste_produits.html', {'produits': produits})
